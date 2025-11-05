5be41f8f560e325a15ac0833481dd8e3df56808c
meeteedee <meepoohfisho@gmail.com>
2025-11-05 12:36:23 +0700

    asset new health

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int currentScore;
+
+    public void AddScore(int scoreAmount)
+    {
+        currentScore += scoreAmount;
+    }
+         
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
